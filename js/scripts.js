e4828737b312fefdd3bd25d9ea1cd7ef0bea64e2
faustlarsen <faustlarsen@gmail.com>
2020-10-21 16:57:45 -0500

    add business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,29 @@
-$(document).ready(function()) {
-  $("#form-group").submit(function(event) {
+// business logic:
+
+
+
+
+
+
+// user interface logic:
+
+$(document).ready(function(){
+  $("#name").submit(function(event) {
     event.preventDefault();
     const firstName = $("input#firstName").val();
-    const secondName = $("input#secondName").val();
+    const secondName = $("input#lastName").val();
     const problem = $("input#problem").val();
     const time = $("#time").val();
+    const date = $("#date").val();
 
-  }
+
+    $(".firstName").text(firstName);
+    $(".lastName").text(secondName);
+    $(".problem").text(problem);
+    $(".time").text(time);
+    $(".date").text(date);
+    
+    $("#submission").show();
+
+  });
 });
