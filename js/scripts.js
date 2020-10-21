c460fe82b01a835922da8bf4c9622cd99bfe64ac
faustlarsen <faustlarsen@gmail.com>
2020-10-21 16:45:52 -0500

    add initial JS
    Co-authored-by: Chelsea Becker <cschweig2@gmail.com>
    Co-authored-by: Max Brockbank <maxbrockbank1999@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
- 
+$(document).ready(function()) {
+  $("#form-group").submit(function(event) {
+    event.preventDefault();
+    const firstName = $("input#firstName").val();
+    const secondName = $("input#secondName").val();
+    const problem = $("input#problem").val();
+    const time = $("#time").val();
+
+  }
+});
